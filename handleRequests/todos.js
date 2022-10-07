97a9a6d06d7519b05799e2922accd5c72a512d7f
Markhan1 <markhan1@protonmail.com>
2022-10-07 13:36:34 +0800

    removed node_modules and modified .gitignore

--- handleRequests/todos.js
+++ handleRequests/todos.js
@@ -4,6 +4,8 @@ const todos = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = dbo.getObjectId();
 
+const dayjs = require("dayjs");
+
 const collec = "todos";
 
 todos
@@ -16,8 +18,11 @@ todos
       .find({})
       .toArray((error, result) => {
         if (error) throw error;
-        console.log("retrieved all documents.");
         res.json(result);
+        let time = dayjs().format("HH:mm:ss:SSS");
+        console.log(
+          `${time} - Retrieved all documents from "${collec}" - ip: "${req.ip}"`
+        );
       });
   })
 
@@ -29,21 +34,118 @@ todos
       .collection(collec)
       .findOne(query, (error, result) => {
         if (error) throw error;
-        console.log(`Retrieved one document by id: ${req.params.id}`);
         res.json(result);
+        let time = dayjs().format("HH:mm:ss:SSS");
+        console.log(
+          `${time} - Retrieved document "${req.params.id}" from "${collec}" - ip: "${req.ip}"`
+        );
       });
   })
 
   // This section helps you create a new record
   .post("/add", (req, res) => {
+    if(!(req.body.name && req.body.text && req.body.status)) {
+      res.json({ message: "missing data" });
+      let time = dayjs().format("HH:mm:ss:SSS");
+      console.log(`${time} - Insufficient data to add newdDocument in "${collec}"`);
+    }
     let db_connect = dbo.getDB();
+    let dateTime = dayjs().format("YYYY/MM/DD HH:mm:ss");
     let newDoc = {
       name: req.body.name,
       text: req.body.text,
       status: req.body.status,
       tags: req.body.tags || [],
-      date: req.body.tags || null,
+      date: req.body.tags || dateTime,
+    };
+    db_connect
+      .collection(collec)
+      .insertOne(newDoc, (error, result) => {
+        if (error) throw error;
+        res.json(result);
+        let time = dayjs().format("HH:mm:ss:SSS");
+        console.log(
+          `${time} - Added new document "${result.insertedId}" to "${collec}" - ip: "${req.ip}"`
+        );
+      });
+  })
+
+  // This section helps you update a single document by id
+  .patch("/update/:id", (req, res) => {
+    if(!(req.body.name && req.body.text && req.body.status)) {
+      res.json({ message: "required data missing... (name, text, status)" });
+      let time = dayjs().format("HH:mm:ss:SSS");
+      console.log(`${time} - Insufficient data to add newdDocument in "${collec}"`);
     }
+    let db_connect = dbo.getDB();
+    let query = { _id: ObjectId(req.params.id) };
+    let dateTime = dayjs().format("YYYY/MM/DD HH:mm:ss");
+    let changes = {
+      $set : {
+        name: req.body.name,
+        text: req.body.text,
+        status: req.body.status,
+        tags: req.body.tags || [],
+        date: req.body.date || dateTime,
+      }
+    };
+    db_connect
+      .collection(collec)
+      .updateOne(query, changes, (error, result) => {
+        if (error) throw error;
+        res.json(result);
+        let time = dayjs().format("HH:mm:ss:SSS");
+        console.log(
+          `${time} - Updated document "${req.params.id}" from "${collec}" - ip: "${req.ip}"`
+        );
+      });
+  })
+
+  // This section also updates a single document by id
+  .post("/update/:id", (req, res) => {
+    if(!(req.body.name && req.body.text && req.body.status)) {
+      res.json({ message: "required data missing... (name, text, status)" });
+      let time = dayjs().format("HH:mm:ss:SSS");
+      console.log(`${time} - Insufficient data to add newdDocument in "${collec}"`);
+    }
+    let db_connect = dbo.getDB();
+    let query = { _id: ObjectId(req.params.id) };
+    let dateTime = dayjs().format("YYYY/MM/DD HH:mm:ss");
+    let changes = {
+      $set : {
+        name: req.body.name,
+        text: req.body.text,
+        status: req.body.status,
+        tags: req.body.tags || [],
+        date: req.body.date || dateTime,
+      }
+    };
+    db_connect
+      .collection(collec)
+      .updateOne(query, changes, (error, result) => {
+        if (error) throw error;
+        res.json(result);
+        let time = dayjs().format("HH:mm:ss:SSS");
+        console.log(
+          `${time} - Updated document "${req.params.id}" from "${collec}" - ip: "${req.ip}"`
+        );
+      });
+  })
+
+  // This section will help you delete a single document by id
+  .delete("/:id", (req, res) => {
+    let db_connect = dbo.getDB();
+    let query = { _id: ObjectId(req.params.id) };
+    db_connect
+      .collection(collec)
+      .deleteOne(query, (error, result) => {
+        if (error) throw error;
+        res.json(result);
+        let time = dayjs().format("HH:mm:ss:SSS");
+        console.log(
+          `${time} - Deleted document "${req.params.id}" from "${collec}" - ip: "${req.ip}"`
+        );
+      });
   });
 
 module.exports = todos;
